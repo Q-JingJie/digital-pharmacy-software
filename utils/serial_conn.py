e0288a01e0654503fe3b72c0656d5078abf8f625
hornzymush <55294093+hornzymush@users.noreply.github.com>
2020-03-09 02:13:25 +0800

    Updated serial_conn and motion_controller

--- utils/serial_conn.py
+++ utils/serial_conn.py
@@ -8,13 +8,13 @@ class serial_conn:
         self.timeout  = time_out
         self.name     = device_name
         Connected = False
-        while (Connected == False):
+        while not Connected:
             try:
                 self.ser = serial.Serial(self.port, baudrate = self.baudrate, timeout = self.timeout)
                 Connected = True
             except serial.serialutil.SerialException:
-                print (self.name, "on", self.port, "is not found!")
-                time.sleep(self.timeout)
+                print (self.name, "on", self.port, "is not found")
+                time.sleep(1)
         
     def read(self):
         status = "Disconnected\n"
@@ -23,7 +23,7 @@ class serial_conn:
         except serial.serialutil.SerialException:
             self.ser.close()
             try:
-                while (self.ser.isOpen() == False):
+                while not self.ser.isOpen():
                     time.sleep(self.timeout)
                     self.ser.open()
             except serial.serialutil.SerialException:
@@ -32,9 +32,26 @@ class serial_conn:
             status = "Decoding Error\n"
         return status
 
+    def set_timeout(self, timeout):
+        self.ser.timeout = timeout
+        self.timeout = timeout
+
+    def flush(self):
+        self.ser.timeout = 0.01
+        while user_qr.read() != '':
+            pass
+        self.ser.timeout = self.timeout
+
     def write(self, data):
         try:
             self.ser.write(data.encode())
             time.sleep(0.01)
         except serial.serialutil.SerialException:
             pass
+    
+    def close(self):
+        try:
+            self.ser.close()
+            print(self.name, "on",self.port, "has been closed")
+        except:
+            print("Error closing", self.name, "on", self.port)
